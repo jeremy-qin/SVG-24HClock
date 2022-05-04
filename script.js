dcf073c6c083b7d3278f52078a73d77730bfae7e
SaulGoodmn <44654549+SaulGoodmn@users.noreply.github.com>
2022-05-04 00:13:10 -0400

    Aiguilles ajoutées. Il manque les phases de lune.

--- script.js
+++ script.js
@@ -49,6 +49,7 @@ function display_time() {
   }
   let min = date.getMinutes();
   let sec = date.getSeconds();
+  let hr = date.getHours();
 
   if (min < 10) {
     min = "0" + min;
@@ -57,13 +58,15 @@ function display_time() {
     sec = "0" + sec;
   }
 
-  var hour = date.getHours() + ":" + min + ":" + sec;
+  let hour = hr + ":" + min + ":" + sec;
 
   document.getElementById("date").innerHTML = day + " " + d + " " + mois[m] + " " + y;
-  document.getElementById("clockhour").innerHTML = hour
-  time = display_t()
+  document.getElementById("clockhour").innerHTML = hour;
+  time = display_t();
+  document.getElementById("aiguille-heure").setAttribute("transform", "rotate(" + (hr * 360 / 24) + " 250 250)");
+  document.getElementById("aiguille-minute").setAttribute("transform", "rotate(" + (min * 360 / 60) + " 250 250)");
+  document.getElementById("aiguille-seconde").setAttribute("transform", "rotate(" + (sec * 360 / 60) + " 250 250)");
 }
-
 
 function formatHeure() {
 
@@ -187,6 +190,7 @@ function chercherTemperaturesC() {
     }
   });
 }
+
 function chercherTemperaturesF() {
   let h = new Date().getHours();
   getJSON('https://api.openweathermap.org/data/2.5/onecall?lat=45.5028&lon=-73.608&units=imperial&lang=fr&appid=f84f3dfb9089d7ca104d69ffca8d8fa0', function (data) {
@@ -198,12 +202,13 @@ function chercherTemperaturesF() {
 }
 
 function chercherMessageJSON() {
-  getJSON('https://www-labs.iro.umontreal.ca/~roys/ift1005/calendrier/test.php?cal=now%2C%2B5sec%2CBienvenue%21%0D%0A%2B5sec%2C%2B5sec%2CHorloge+SVG+r%C3%A9alis%C3%A9+par%0D%0A%2B5sec%2C%2B10sec%2CMehdi+Aqdim%0D%0A%2B15sec%2C%2B20sec%2CJeremy+Qin%0D%0A%2B20sec%2C%2B25sec%2CFrancisco+Lopez+Saavedra', function (data) {
+  getJSON('https://www-labs.iro.umontreal.ca/~roys/ift1005/calendrier/test.php?cal=now%2C%2B5sec%2CBienvenue%21%0D%0A%2B5sec%2C%2B5sec%2CHorloge+SVG+r%C3%A9alis%C3%A9+par%0D%0A%2B5sec%2C%2B10sec%2CMehdi+Aqdim%0D%0A%2B15sec%2C%2B20sec%2CJeremy+Qin%0D%0A%2B20sec%2C%2B30sec%2CFrancisco+Lopez+Saavedra', function (data) {
     for (let message of data) {
       setTimeout(changerMessageTicker(message.message), (message.fin - message.debut) * 1000);
     }
   });
 }
+
 function changerMessageTicker(message) {
   document.getElementById("ticker").innerHTML += '<div class="hitem">' + message + '</div> ';
 }
@@ -228,9 +233,9 @@ function sunState() {
     if (this.readyState == 4 && this.status == 200) {
       var jSunState = JSON.parse(this.responseText);
       var daily = jSunState.daily;
-              // Moon data and Moon function call
-              var moonPhase = daily[1].moon_phase;
-              moonState(moonPhase);
+      // Moon data and Moon function call
+      var moonPhase = daily[1].moon_phase;
+      moonState(moonPhase);
       // sunrise data
       var srDate = new Date(daily[1].sunrise * 1000);
       var srHour = srDate.getHours();
@@ -241,12 +246,12 @@ function sunState() {
       var ssHour = ssDate.getHours();
       var ssMin = ssDate.getMinutes();
       var ssMinForm = ssHour * 60 + ssMin;
-        // Data handling
-        var dayRatio = (ssMinForm-srMinForm)/1440; // Compute the ratio of duration between sunrise and sunset by a day
+      // Data handling
+      var dayRatio = (ssMinForm - srMinForm) / 1440; // Compute the ratio of duration between sunrise and sunset by a day
       // sets the form of our day circle according to the ratio
       document.getElementById("day_circle").setAttribute("stroke-dasharray", (dayRatio * 502) + " 502");
       const degreeOffset = 90;
-               // Positions the day circle
+      // Positions the day circle
       var daystart = ((srMinForm) / 1440) * 360;
       document.getElementById("day_cycle_rotation").setAttribute("to", (daystart - degreeOffset) + " 250 250");
       var rotation = document.getElementById("day_cycle_rotation");
@@ -258,43 +263,45 @@ function sunState() {
 }
 
 // handles data retrieved from Api, changes the html to show the current moon phase
-function moonState(moonphase){
-  if (moonphase == 0 || moonphase == 1 ){
-    document.getElementById("moontxt").innerHTML="New Moon";
+function moonState(moonphase) {
+  if (moonphase == 0 || moonphase == 1) {
+    document.getElementById("moontxt").innerHTML = "New Moon";
     return;
   }
-  if (moonphase == 0.25){
-    document.getElementById("moontxt").innerHTML="First Quarter Moon";
+  if (moonphase == 0.25) {
+    document.getElementById("moontxt").innerHTML = "First Quarter Moon";
     return;
   }
-  if (moonphase == 0.5 ){
-    document.getElementById("moontxt").innerHTML="Full Moon";
+  if (moonphase == 0.5) {
+    document.getElementById("moontxt").innerHTML = "Full Moon";
     return
   }
-  if (moonphase == 0.75){
-    document.getElementById("moontxt").innerHTML="Last Quarter Moon";
+  if (moonphase == 0.75) {
+    document.getElementById("moontxt").innerHTML = "Last Quarter Moon";
     return;
   }
-  if (moonphase < 0.25){
-    document.getElementById("moontxt").innerHTML="Waxing crescent";
+  if (moonphase < 0.25) {
+    document.getElementById("moontxt").innerHTML = "Waxing crescent";
     return;
   }
-  if (moonphase < 0.5){
-    document.getElementById("moontxt").innerHTML="Waxing Gibous";
+  if (moonphase < 0.5) {
+    document.getElementById("moontxt").innerHTML = "Waxing Gibous";
     return;
   }
-  if (moonphase < 0.75){
-    document.getElementById("moontxt").innerHTML="Waning Gibous";
+  if (moonphase < 0.75) {
+    document.getElementById("moontxt").innerHTML = "Waning Gibous";
     return;
   }
-  if (moonphase < 1){
-    document.getElementById("moontxt").innerHTML="Waning Crescent";
+  if (moonphase < 1) {
+    document.getElementById("moontxt").innerHTML = "Waning Crescent";
     return;
   }
 }
 
 
-
-
-
-window.onload(display_time(), chercherMessageJSON(), chercherTemperaturesC(), sunState());
+window.onload = function () {
+  display_time();
+  chercherMessageJSON();
+  chercherTemperaturesC();
+  sunState();
+}
